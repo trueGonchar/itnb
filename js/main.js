b400045a2fe8cd0995679fc9770b5705c2205958
xuyara <58000405+xuyara@users.noreply.github.com>
2020-06-03 17:47:37 +0300

    123
    123

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function backToTop() {
     let button = $('.back-to-top');
 
     $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 700){
+        if ($(this).scrollTop() >= 500){
             button.fadeIn();
         } else {
             button.fadeOut();
